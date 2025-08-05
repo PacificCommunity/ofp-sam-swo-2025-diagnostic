9bdfd240b5857b77b71669344b8878cd5918b1b4
Arni Magnusson <thisisarni@gmail.com>
2025-08-05 17:22:57 +1100

    Add Kobe table

--- TAF/output.R
+++ TAF/output.R
@@ -1,9 +1,24 @@
 # Extract results of interest, write CSV output tables
 
-# Before:
-# After:
+# Before: results.rds (model)
+# After:  kobe.csv (output)
 
 library(TAF)
 
 mkdir("output")
 
+# Read model results
+model <- readRDS("model/model.rds")
+derived <- model$derived_quants
+
+# Extract SB_SBmsy and F_Fmsy
+SB_SBmsy <- derived$Value[grep("Bratio_[12]", derived$Label)]
+SB_SBmsy <- c(SB_SBmsy[1], SB_SBmsy)  # similar in year 1 and year 2
+F_Fmsy <- derived$Value[grep("F_[12]", derived$Label)]
+Year <- as.integer(sub("F_", "", grepv("F_[12]", derived$Label)))
+
+# Construct kobe data frame
+kobe <- data.frame(Year, SB_SBmsy, F_Fmsy)
+
+# Write table
+write.taf(kobe, dir="output")
