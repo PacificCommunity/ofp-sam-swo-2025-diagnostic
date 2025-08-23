ab9918f07440935cfa0fd0c8485d5a10a4343245
Arni Magnusson <thisisarni@gmail.com>
2025-08-24 10:25:49 +1100

    Rename growth.R -> output_biology.R

---
+++ TAF/output_biology.R
@@ -0,0 +1,38 @@
+# Extract biology results of interest, write CSV output tables
+
+# Before: model.rds (model)
+# After:  biology.csv (output)
+
+library(TAF)
+library(r4ss)
+library(fishgrowth)
+
+mkdir("output")
+
+# Read model results
+model <- readRDS("model/model.rds")
+biology <- model$biology
+endgrowth <- model$endgrowth
+Growth_Parameters <- model$Growth_Parameters
+Natural_Mortality <- model$Natural_Mortality
+parameters <- model$parameters
+
+# Extract growth parameters
+L1f <- parameters$Value[parameters$Label == "L_at_Amin_Fem_GP_1"]
+L2f <- parameters$Value[parameters$Label == "L_at_Amax_Fem_GP_1"]
+kf <- parameters$Value[parameters$Label == "VonBert_K_Fem_GP_1"]
+L1m <- L1f * exp(parameters$Value[parameters$Label == "L_at_Amin_Mal_GP_1"])
+L2m <- L2f * exp(parameters$Value[parameters$Label == "L_at_Amax_Mal_GP_1"])
+km <- kf * exp(parameters$Value[parameters$Label == "VonBert_K_Mal_GP_1"])
+t1 <- Growth_Parameters$A1[1]
+t2 <- Growth_Parameters$A2[1]
+
+# Calculate growth curve
+out <- data.frame(Age=seq(0, 19, 0.25))
+out$Lf <- vonbert_curve(out$Age, L1f, L2f, kf, t1, t2)
+out$Lm <- vonbert_curve(out$Age, L1m, L2m, km, t1, t2)
+# plot(Len_Beg~Real_Age, endgrowth)
+# matlines(out[1], out[-1], col=c(2,4), lty=1, lwd=5)
+
+# Write table
+write.taf(out, "output/biology.csv")
---
+++ TAF/output_summary.R
@@ -0,0 +1,35 @@
+# Extract results of interest, write CSV output tables
+
+# Before: model.rds (model)
+# After:  kobe.csv, majuro.csv (output)
+
+library(TAF)
+
+mkdir("output")
+
+# Read model results
+model <- readRDS("model/model.rds")
+annual <- model$annual_time_series
+derived <- model$derived_quants
+dynamic <- model$Dynamic_Bzero[model$Dynamic_Bzero$Era == "TIME",]
+
+# Extract SB_SBmsy and F_Fmsy
+Year <- annual$year
+SB <- annual$SSB
+SBmsy <- derived$Value[derived$Label == "SSB_MSY"]
+SB_SBmsy <- SB / SBmsy
+Fmort <- annual$"F=Z-M"
+Fmsy <- derived$Value[derived$Label == "annF_MSY"]
+F_Fmsy <- Fmort / Fmsy
+
+# Extract SB_SBF0
+SB_SBF0 <- dynamic$SSB / dynamic$SSB_nofishing
+
+# Construct kobe and majuro data frames
+summary <- data.frame(Year, SB, F=Fmort, SB_SBmsy, F_Fmsy, SB_SBF0)
+kobe <- data.frame(Year, SB_SBmsy, F_Fmsy)
+majuro <- data.frame(Year, SB_SBF0, F_Fmsy)
+
+# Write table
+write.taf(kobe, dir="output")
+write.taf(majuro, dir="output")
