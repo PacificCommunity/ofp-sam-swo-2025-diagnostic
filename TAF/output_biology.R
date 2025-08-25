9d0eecfe271bb05b7ac6c513bc1e3d44c81d93d3
Arni Magnusson <thisisarni@gmail.com>
2025-08-25 11:27:03 +1100

    Rename biology columns

--- TAF/output_biology.R
+++ TAF/output_biology.R
@@ -1,4 +1,4 @@
-# Extract biology results of interest, write CSV output tables
+# Extract biology results, write CSV output tables
 
 # Before: model.rds (model)
 # After:  biology.csv (output)
@@ -12,11 +12,13 @@ mkdir("output")
 model <- readRDS("model/model.rds")
 endgrowth <- model$endgrowth
 
-# Biology
+# Construct biology table
 biology <- subset(endgrowth, Seas==1 & Settlement==1,
                   c("Sex", "Age_Beg", "M", "Len_Beg", "Wt_Beg", "Age_Mat"))
 names(biology) <- sub("_Beg", "", names(biology))
 names(biology) <- sub("Age_", "", names(biology))
+biology[biology < 0] <- NA
+row.names(biology) <- NULL
 
 # Write table
 write.taf(biology, dir="output")
