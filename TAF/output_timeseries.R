e9cc485ac2f89f1cde418eff61e3fbec9ff113bd
Arni Magnusson <thisisarni@gmail.com>
2025-08-25 18:41:03 +1100

    Add output_timeseries

--- TAF/output_timeseries.R
+++ TAF/output_timeseries.R
@@ -1,7 +1,7 @@
-# Extract summary time series, write CSV output tables
+# Extract F and stock by area, write CSV output tables
 
 # Before: model.rds (model)
-# After:  summary.csv (output)
+# After:  f_annual.csv, f_season.csv, stock_by_area.csv (output)
 
 library(TAF)
 
@@ -9,11 +9,35 @@ mkdir("output")
 
 # Read model results
 model <- readRDS("model/model.rds")
+timeseries <- model$timeseries[model$timeseries$Era == "TIME",]
 
 # Calculate time series
-timeseries <- model$timeseries
-timeseries <- timeseries[timeseries$Era == "TIME",]
-timeseries <- timeseries[timeseries$Seas == 1,]
+stock.by.area <- timeseries[timeseries$Seas == 1,]
+names(stock.by.area)[names(stock.by.area) == "Recruit_0"] <- "Rec"
+names(stock.by.area)[names(stock.by.area) == "Bio_all"] <- "TB"
+names(stock.by.area)[names(stock.by.area) == "SpawnBio"] <- "SB"
+stock.by.area <- stock.by.area[c("Area", "Yr", "Rec", "TB", "SB")]
+row.names(stock.by.area) <- NULL
 
-timeseries$Era <- NULL
-timeseries$Seas <- NULL
+# Calculate F by season
+f.season <- timeseries
+cols <- c("Area", "Yr", "Seas")
+f.season <- f.season[c(cols, grepv("F:_", names(f.season)))]
+names(f.season) <- sub("F:_", "", names(f.season))
+rowlab <- apply(f.season[c("Area", "Yr", "Seas")], 1, paste, collapse = "|")
+f.season <- f.season[!names(f.season) %in% cols]
+row.names(f.season) <- rowlab
+f.season <- xtab2long(f.season, c("AYS", "Age", "F"))
+right <- f.season[c("Age", "F")]
+left <- read.table(text=f.season$AYS, sep = "|", col.names=cols)
+f.season <- cbind(left, right)
+
+# Calculate F by year
+f.annual <- aggregate(F~Area+Yr+Age, f.season, sum)
+f.annual <- f.annual[order(f.annual$Area, f.annual$Age, f.annual$Yr),]
+row.names(f.annual) <- NULL
+
+# Write tables
+write.taf(f.annual, dir="output")
+write.taf(f.season, dir="output")
+write.taf(stock.by.area, dir="output")
