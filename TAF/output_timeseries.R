c7440f449c34af62ef283e228dcd202006a974cf
Arni Magnusson <thisisarni@gmail.com>
2025-08-26 21:04:24 +1100

    Calculate F by fleet

--- TAF/output_timeseries.R
+++ TAF/output_timeseries.R
@@ -1,7 +1,7 @@
 # Extract F and stock by area, write CSV output tables
 
 # Before: model.rds (model)
-# After:  f_annual.csv, f_season.csv, stock_by_area.csv (output)
+# After:  f_annual.csv, f_fleet.csv, f_season.csv, stock_by_area.csv (output)
 
 library(TAF)
 
@@ -9,6 +9,7 @@ mkdir("output")
 
 # Read model results
 model <- readRDS("model/model.rds")
+exploitation <- model$exploitation
 timeseries <- model$timeseries[model$timeseries$Era == "TIME",]
 
 # Calculate time series
@@ -30,7 +31,15 @@ f.annual <- aggregate(F~Area+Yr+Age, f.season, sum)
 f.annual <- f.annual[order(f.annual$Age, f.annual$Area, f.annual$Yr),]
 row.names(f.annual) <- NULL
 
+# Calcualte F by fleet
+exploitation$Seas_dur <- NULL
+exploitation$F_std <- NULL
+exploitation$annual_F <- NULL
+exploitation$annual_M <- NULL
+f.fleet <- wide2long(exploitation, names=c("Fleet", "F"))
+
 # Write tables
 write.taf(f.annual, dir="output")
+write.taf(f.fleet, dir="output")
 write.taf(f.season, dir="output")
 write.taf(stock.by.area, dir="output")
