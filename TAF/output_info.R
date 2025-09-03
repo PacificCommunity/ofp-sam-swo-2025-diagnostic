c9f29fe86c32397bbbb4cb98962665160e6c0a14
Arni Magnusson <thisisarni@gmail.com>
2025-09-03 13:29:11 +1100

    Merge output_catch into output_info

--- TAF/output_info.R
+++ TAF/output_info.R
@@ -1,7 +1,8 @@
 # Extract results of interest, write CSV output tables
 
 # Before: model.rds (model)
-# After:  biology.csv, likelihoods.csv, movement.csv, stats.csv (output)
+# After:  biology.csv, catch.csv, likelihoods.csv, movement.csv,
+#         stats.csv (output)
 
 library(TAF)
 library(r4ss)
@@ -10,6 +11,7 @@ mkdir("output")
 
 # Read model results
 model <- readRDS("model/model.rds")
+catch <- model$catch
 endgrowth <- model$endgrowth
 likelihoods <- model$likelihoods_used
 movement <- model$movement
@@ -21,6 +23,12 @@ names(biology) <- sub("_Beg", "", names(biology))
 names(biology) <- sub("Age_", "", names(biology))
 biology[biology < 0] <- NA
 row.names(biology) <- NULL
+
+# Catch
+catch <- catch[c("Fleet", "Fleet_Name", "Area", "Yr", "Seas", "dead_bio")]
+catch <- catch[catch$Yr >= model$startyr,]
+names(catch)[names(catch) == "dead_bio"] <- "Catch"
+row.names(catch) <- NULL
 
 # Likelihoods
 likelihoods <- likelihoods[likelihoods$values != 0,]
@@ -42,6 +50,7 @@ stats <- data.frame(npar, objfun, gradient, start, runtime, version)
 
 # Write tables
 write.taf(biology, dir="output")
+write.taf(catch, dir="output")
 write.taf(likelihoods, dir="output")
 write.taf(movement, dir="output")
 write.taf(stats, dir="output", quote=TRUE)
